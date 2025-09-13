6aa31a8bcd4b0b035710906dc132431909a088cc
stephenc01 <stephenc01@users.noreply.github.com>
2025-09-13 15:41:48 -0400

    Update docker-bw-init.sh

--- docker/docker-bw-init.sh
+++ docker/docker-bw-init.sh
@@ -114,9 +114,9 @@ check_env() {
 }
 
 check_env
-ensure_privsep_dir
+#ensure_privsep_dir
 init_ssh_server
-#check_ssh_directory
+check_ssh_directory
 create_authorized_keys_file
 check_repos_directory
 maybe_start_haveged
